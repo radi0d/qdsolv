b3fe9608e5a5c22a282c7cc6c0947ecab6c8750e
radi0d <319424863+radi0d@users.noreply.github.com>
2026-08-21 16:38:15 +0300

    style: yoda notation

--- src/input.cpp
+++ src/input.cpp
@@ -37,7 +37,7 @@ get_double(double *dst)
 	char mark = ' ';
 	ssize_t num = sscanf(line, "%lf %c", &t, &mark);
 
-	if (num != 1 || isnan(t) || isinf(t) || !isspace(mark)) {
+	if (1 != num || isnan(t) || isinf(t) || !isspace(mark)) {
 		free(line);
 		return -1;
 	}
